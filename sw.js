8d9c1e41dea9363a367226d4870c3dddf10c4aad
Mikymallorca007 <Mikymallorca@gmail.com>
2026-03-09 16:19:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('fetch', (e) => { });
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+    // Esto responde con los archivos de la red, 
+    // cumpliendo el requisito para ser una PWA instalable.
+    e.respondWith(fetch(e.request));
+});
